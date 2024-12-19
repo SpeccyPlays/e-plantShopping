aa75bf1fe1fc4659579f00d74a7bc0004a53916b
SpeccyPlays <lilben13@googlemail.com>
2024-12-19 18:50:49 +0000

    Fixed addtocart error and added number values to each product so total amount could be calculated

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,20 +9,29 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+      let totalAmount = 0;
+      
+      cart.forEach((item) => {
+        console.log(item);
+        totalAmount += item.quantity * item.value;
+      })
+      return totalAmount;
   };
 
   const handleContinueShopping = (e) => {
-   
+    console.log(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    updateQuantity(item);
   };
 
   const handleDecrement = (item) => {
-   
+    updateQuantity(item);
   };
 
   const handleRemove = (item) => {
@@ -57,7 +66,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
